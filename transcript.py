582912703887be804fbec65a106db7db7c16418b
= <=>
2023-05-31 12:33:04 +0530

    removed function

--- transcript.py
+++ transcript.py
@@ -28,37 +28,36 @@ for video_id in video_links:
             f.write(f"{line['text']}\n")
 
 
-def chat(question):
-    OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
-    loader = DirectoryLoader(path='./', glob = "**/*.txt", loader_cls=TextLoader,
-                            show_progress=True)
 
-    index = VectorstoreIndexCreator(embedding=OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)).from_loaders([loader])
-    retriever = index.vectorstore.as_retriever(search_kwargs=dict(k=5))
-    memory = VectorStoreRetrieverMemory(retriever=retriever)
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+loader = DirectoryLoader(path='./', glob = "**/*.txt", loader_cls=TextLoader,
+                        show_progress=True)
 
-    llm = OpenAI(temperature=0.7, openai_api_key=OPENAI_API_KEY) # Can be any valid LLM
-    _DEFAULT_TEMPLATE = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+index = VectorstoreIndexCreator(embedding=OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)).from_loaders([loader])
+retriever = index.vectorstore.as_retriever(search_kwargs=dict(k=5))
+memory = VectorStoreRetrieverMemory(retriever=retriever)
 
-    Relevant pieces of previous conversation:
-    {history}
+llm = OpenAI(temperature=0.7, openai_api_key=OPENAI_API_KEY) # Can be any valid LLM
+_DEFAULT_TEMPLATE = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
 
-    (You do not need to use these pieces of information if not relevant)
+Relevant pieces of previous conversation:
+{history}
 
-    Current conversation:
-    Human: {input}
-    AI:"""
-    PROMPT = PromptTemplate(
-        input_variables=["history", "input"], template=_DEFAULT_TEMPLATE
-    )
-    conversation_with_summary = ConversationChain(
-        llm=llm, 
-        prompt=PROMPT,
-        # We set a very low max_token_limit for the purposes of testing.
-        memory=memory,
-    )
+(You do not need to use these pieces of information if not relevant)
 
-    return conversation_with_summary.predict(input = question)
+Current conversation:
+Human: {input}
+AI:"""
+PROMPT = PromptTemplate(
+    input_variables=["history", "input"], template=_DEFAULT_TEMPLATE
+)
+conversation_with_summary = ConversationChain(
+    llm=llm, 
+    prompt=PROMPT,
+    # We set a very low max_token_limit for the purposes of testing.
+    memory=memory,
+)
+
 
 
 st.set_page_config(
@@ -67,7 +66,7 @@ st.set_page_config(
 )
 
 st.header("YouTubeGPT")
-st.markdown("[Github](https://github.com/ai-yash/st-chat)")
+st.markdown("[Github](https://github.com/akshayballal95)")
 
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
@@ -82,7 +81,7 @@ def get_text():
 user_input = get_text()
 
 if user_input:
-    output = chat(user_input)
+    output = conversation_with_summary.predict(input = user_input)
 
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output)
